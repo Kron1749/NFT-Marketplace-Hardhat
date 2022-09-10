562dff15fe9c9221ee4410d9f95d163c7b9911df
Kron1749 <70746258+Kron1749@users.noreply.github.com>
2022-09-10 11:27:15 +0300

    Created move blocks script

--- scripts/mint-and-list.js
+++ scripts/mint-and-list.js
@@ -1,4 +1,5 @@
-const { ethers } = require("hardhat")
+const { ethers, network } = require("hardhat")
+const { moveBlocks, sleep } = require("../utils/move-blocks")
 
 const PRICE = ethers.utils.parseEther("0.1")
 
@@ -16,6 +17,10 @@ async function mintAndList() {
     const tx = await NFTMarketplace.listItem(basicNft.address, tokenId, PRICE)
     await tx.wait(1)
     console.log("Listed")
+
+    if (network.config.chainId == "31337") {
+        await moveBlocks(1, (sleepAmount = 1000))
+    }
 }
 
 mintAndList()
